1694d016fccd5a0ba5478ab0920dc29ee55dd88f
Juan Roldan <juanroldan1989@gmail.com>
2018-05-13 03:12:34 -0300

    `Game` model - `mines_positions` randomly generated

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,6 +1,8 @@
 class Game < ActiveRecord::Base
   validates :rows, :columns, :mines, presence: true
 
+  before_create :generate_mines_positions
+
   PLAYING = 0
   PAUSED  = 1
   LOST    = 2
@@ -15,5 +17,20 @@ class Game < ActiveRecord::Base
 
   def status_name
     STATUSES.rassoc(self.status).try(:first) || ""
+  end
+
+  private
+
+  def generate_mines_positions
+    self.mines.times do
+      mine_position = [rand(rows), rand(columns)]
+
+      # making sure mines are not overlapping within each other
+      unless self.mines_positions.include?(mine_position)
+        mine_position = [rand(rows), rand(columns)]
+      end
+
+      self.mines_positions += [mine_position]
+    end
   end
 end
