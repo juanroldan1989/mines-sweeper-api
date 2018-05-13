0838b967be6aad47d74d117c70b662ada26412b3
Juan Roldan <juanroldan1989@gmail.com>
2018-05-13 01:32:37 -0300

    `Game` model - `statuses` list updated

--- app/models/game.rb
+++ app/models/game.rb
@@ -2,12 +2,18 @@ class Game < ActiveRecord::Base
   validates :rows, :columns, :mines, presence: true
 
   PLAYING = 0
-  LOST    = 1
-  WON     = 2
+  PAUSED  = 1
+  LOST    = 2
+  WON     = 3
 
   STATUSES = [
     ["Playing", PLAYING],
+    ["Paused",  PAUSED],
     ["Lost",    LOST],
     ["Won",     WON]
   ]
+
+  def status_name
+    STATUSES.rassoc(self.status).try(:first) || ""
+  end
 end
