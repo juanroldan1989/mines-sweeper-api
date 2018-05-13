0928cf8d15b0f0073508ca7cc043b664db761b6b
Juan Roldan <juanroldan1989@gmail.com>
2018-05-13 01:54:41 -0300

    `MineSweeper` JS library - `drawGame` function implemented

--- app/assets/javascripts/mines_sweeper_client.js
+++ app/assets/javascripts/mines_sweeper_client.js
@@ -1,6 +1,6 @@
-console.log("MineSweeper JS library loaded!")
+console.log("MineSweeper JS library loaded!");
 
-var VERSION     = "v1"
+var VERSION     = "v1";
 var MineSweeper = (function ($) {
   return {
     createGame: function(rows, columns, mines) {
@@ -29,11 +29,29 @@ var MineSweeper = (function ($) {
         },
         success  : function(data){
           console.log(data);
+          MineSweeper.drawGame(data);
         },
         error : function(data){
           console.log(data);
         }
       });
+    },
+    drawGame: function(gameData) {
+      var rows            = gameData.rows;
+      var columns         = gameData.columns;
+      var minesPositions  = gameData.mines_positions;
+      var squaresRevealed = gameData.squares_revealed;
+      var squaresFlagged  = gameData.squares_flagged;
+
+      var gameTable = $("#game");
+
+      for (var i = 0; i < rows; i++) {
+        var row = $('<tr></tr>').appendTo(gameTable);
+        for (var j = 0; j < columns; j++) {
+          $("<td data-row=" + i + " data-column=" + j +" class='callout'></td>").text("").appendTo(row);
+        }
+      }
+      // console.log("gameTable:" + gameTable.html());
     }
   };
 })(jQuery);
