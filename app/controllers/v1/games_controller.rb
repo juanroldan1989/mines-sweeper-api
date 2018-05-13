d1f46a01c1b29de44b51ad2d1cff98181407e888
Juan Roldan <juanroldan1989@gmail.com>
2018-05-13 01:29:35 -0300

    API versioning applied to `games` controller

---
+++ app/controllers/v1/games_controller.rb
@@ -0,0 +1,32 @@
+module V1
+  class GamesController < ApplicationController
+
+    def create
+      game = Game.create(resource_params)
+
+      if game.valid?
+        render json: { status: 200, id: game.id }
+      else
+        render json: { status: 400, message: "Game can't be created", errors: game.errors.full_messages }
+      end
+    end
+
+    def show
+      game = Game.find_by_id(params[:id])
+
+      if game.present?
+        render json: game.as_json(only:
+          [:id, :rows, :columns, :mines, :mines_positions, :squares_revealed, :squares_flagged]
+        ).merge({ started_at: game.created_at, stopped_at: game.stopped_at, status: game.status_name })
+      else
+        render json: { status: 404, message: "Game not found" }
+      end
+    end
+
+    private
+
+    def resource_params
+      params.require(:game).permit(:rows, :columns, :mines)
+    end
+  end
+end
