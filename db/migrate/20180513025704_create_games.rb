5a3192ced2c4005e1aa49313d95c2d5eb8c04702
Juan Roldan <juanroldan1989@gmail.com>
2018-05-13 00:32:24 -0300

    `games` table - `stopped_at` column type updated

--- db/migrate/20180513025704_create_games.rb
+++ db/migrate/20180513025704_create_games.rb
@@ -1,14 +1,14 @@
 class CreateGames < ActiveRecord::Migration
   def change
     create_table :games do |t|
-      t.integer :rows
-      t.integer :columns
-      t.integer :mines
-      t.text    :mines_positions, array: true, default: []
-      t.text    :squares_revealed, array: true, default: []
-      t.text    :squares_flagged, array: true, default: []
-      t.time    :stopped_at
-      t.integer :status, default: 0
+      t.integer  :rows
+      t.integer  :columns
+      t.integer  :mines
+      t.text     :mines_positions, array: true, default: []
+      t.text     :squares_revealed, array: true, default: []
+      t.text     :squares_flagged, array: true, default: []
+      t.datetime :stopped_at
+      t.integer  :status, default: 0
 
       t.timestamps null: false
     end
